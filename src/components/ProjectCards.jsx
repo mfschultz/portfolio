6b9ad69a92eee1d76ee3414c9e65229d94dea852
Trey Schultz <mfschultz3@gmail.com>
2024-05-22 23:13:47 -0400

    Projects Update
    - added previous work experience
    - removed future projects that are not started
    - Formatting Changes to include dates of working on projects
    - added images for NumbersUSA and SecureFlight
    - removed unused images

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -2,46 +2,46 @@ import styles from '../styles/ProjectCards.module.css';
 import buttonStyle from '../styles/Button.module.css';
 import React from 'react';
 import kcLogo from '../assets/kcLogo.png';
-import moneyLogo from '../assets/moneyApp.png';
-import deliveranceLogo from '../assets/deliveranceLogo.png';
+import nusaLogo from '../assets/numbersUSA.jpg';
+import sfLogo from '../assets/secureFlight.jpg';
 import { NavLink } from 'react-router-dom';
 
 function ProjectCards() {
   return (
     <div className={styles.projects}>
-      <h1 className={styles['project-heading']}>Projects</h1>
+      <h1 className={styles['project-heading']}>Past and Present Projects</h1>
       <div className={styles['card-container']}>
         <div className={styles.card}>
             <img src={kcLogo} alt='image' />
-            <h2 className={styles['project-title']}>Kingdom Connect</h2>
+            <h2 className={styles['project-title']}>KingdomConnect</h2>
             <div className={styles['project-details']}>
-              <p>-Coming Soon-</p>
+              <h5>-Coming Soon-</h5>
+              <p>KingdomConnect serves as hub to connect Christians with businesses submitted under the authority of Jesus Christ.  All member businesses take a pledge, that when violated, can result in the termination of their membership.  Through this accountability members will grow in honor, discipline, stewardship and a love for God and the people He created in His likeness.</p>
             </div>
             <div className={styles['project-btns']}>
-              <NavLink to='url.com' className={`${buttonStyle.btn} ${styles.btn}`}>View</NavLink>
-              <NavLink to='https://github.com/mfschultz/Kingdom_Connect_API' className={`${buttonStyle.btn} ${styles.btn}`}>Source</NavLink>
+              <NavLink to='https://kingdom-connect.com' className={`${buttonStyle.btn} ${styles.btn}`}>View</NavLink>
             </div>
         </div>
         <div className={styles.card}>
-        <img src={moneyLogo} alt='image' />
-            <h2 className={styles['project-title']}>My.Money</h2>
+        <img src={nusaLogo} alt='image' />
+            <h2 className={styles['project-title']}>NumbersUSA</h2>
             <div className={styles['project-details']}>
-              <p>-Coming Soon-</p>
+              <h5>-October 2019 to December 2021-</h5>
+              <p>NumbersUSA provides tools, resources, and access, so that your voice is heard by the Federal and State elected officials who represent you. They provide information on issues, current immigration numbers, voting record “Grade Cards”, and an constantly updated “Action Board” filled with what actions you can take.</p>
             </div>
             <div className={styles['project-btns']}>
-              <NavLink to='url.com' className={`${buttonStyle.btn} ${styles.btn}`}>View</NavLink>
-              <NavLink to='url.com' className={`${buttonStyle.btn} ${styles.btn}`}>Source</NavLink>
+              <NavLink to='https://www.numbersusa.com/' className={`${buttonStyle.btn} ${styles.btn}`}>View</NavLink>
             </div>
         </div>
         <div className={styles.card}>
-        <img src={deliveranceLogo} alt='image' />
-            <h2 className={styles['project-title']}>Deliver Me</h2>
+        <img src={sfLogo} alt='image' />
+            <h2 className={styles['project-title']}>SecureFlight</h2>
             <div className={styles['project-details']}>
-              <p>-Coming Soon-</p>
+              <h5>-March 2015 to August 2019-</h5>
+              <p>The purpose of the Secure Flight program is to screen individuals before they access airport sterile areas or board aircraft.  This screening is designed to identify individuals who may be a threat to transportation or national security and to ensure enhanced screening prior to boarding an aircraft.</p>
             </div>
             <div className={styles['project-btns']}>
-              <NavLink to='url.com' className={`${buttonStyle.btn} ${styles.btn}`}>View</NavLink>
-              <NavLink to='url.com' className={`${buttonStyle.btn} ${styles.btn}`}>Source</NavLink>
+              <NavLink to='https://www.dhs.gov/publication/dhstsapia-018-tsa-secure-flight' className={`${buttonStyle.btn} ${styles.btn}`}>View</NavLink>
             </div>
         </div>
       </div>
